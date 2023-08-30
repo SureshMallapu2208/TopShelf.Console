f5eeab969fc6459cb6ed2b48779a280eab172368
sureshmallapu2208 <142003202+SureshMallapu2208@users.noreply.github.com>
2023-08-30 19:16:23 +0530

    Update Configuration.cs

--- TopShelf.Console/Configuration.cs
+++ TopShelf.Console/Configuration.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using Topshelf;
 
 namespace TopShelf.Console
 {
-    internal class Configuration
+    internal static class Configuration
     {
         public static void Configure()
         {
             HostFactory.Run(configure =>
             {
-                configure.Service<Service>();
-                configure.SetStartTimeout(new TimeSpan(2000));
-                configure.SetStopTimeout(new TimeSpan(4000));
+                configure.Service<Service>(service =>
+                {
+                    service.ConstructUsing(s => new Service());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
 
                 //Setup Account that window service use to run.  
                 configure.RunAsLocalSystem();
                 configure.SetServiceName("WinServiceWithTopshelf");
                 configure.SetDisplayName("WinServiceWithTopshelf");
                 configure.SetDescription("WinService With Topshelf");
+                configure.EnableServiceRecovery(r => r.RestartService(TimeSpan.FromSeconds(10)));
+                configure.StartAutomatically();
             });
         }
     }
